a6232dcdb03ab1e69cde68e99351e3ba323eb527
Robert <robroyal@gmx.net>
2010-03-07 18:00:29 +0100

    Codesbasis zusammengeführt und Encoding geändert

--- core.lua
+++ core.lua
@@ -1,30 +1,30 @@
--- Einstellungen
+﻿-- Einstellungen
 local BiteMe = {}
-BiteMe[1] = {
+BiteMe["Left"] = {
 	"L1", --L1
 	"L2", --L2
 	"Knopf", --L3
 	"L4",  --L4
 	"L5"  --L5
 }
-BiteMe[2] = {
+BiteMe["Right"] = {
 	"R1", --R1
 	"R2", --R2
 	"R3", --R3
 	"R4", --R4
 	"R5"  --R5
 }
-local RangedPlayer = "RP1"
-local RangedPlayer = "RP2"
-local Prefix = "BiteME: " -- Pr�fix f�r z.B. die Whispers
+local RangedPlayer1 = "RP1"
+local RangedPlayer2 = "RP2"
+local Prefix = "BiteME: " -- Pröfix für z.B. die Whispers
 
 -- alle unsere Combatlog Daten
 local timestamp, type, srcGUID, srcName, srcFlgs, dstGUID, dstName, dstFlgs, spellID, spellName
 
 -- Frame bauen
-local BiteMELEE = CreateFrame("FRAME", "BiteMELEE")
+local BiteME = CreateFrame("FRAME", "BiteME")
 
-BiteMELEE:SetScript("OnEvent",
+BiteME:SetScript("OnEvent",
 	function(self, event, ...)
 		if event == "COMBAT_LOG_EVENT_UNFILTERED" then
 			timestamp, type, srcGUID, srcName, srcFlgs, dstGUID, dstName, dstFlgs = select(1, ...)		
@@ -43,15 +43,15 @@ BiteMELEE:SetScript("OnEvent",
 			self:Initialize()
 		end
 	end)	
-BiteMELEE:RegisterEvent("ADDON_LOADED")
+BiteME:RegisterEvent("ADDON_LOADED")
 
-function BiteMELEE:Initialize()	
+function BiteME:Initialize()	
 	self:UnregisterEvent("ADDON_LOADED")
 	-- Slashcommands einbinden
-	SLASH_BITEMELEE1 = "/bitemelee"
-	SLASH_BITEMELEE2 = "/bm"
-	SlashCmdList["BITEMELEE"] = function(msg) self:SlashCommandHandler(msg)	end	
-	self:message("loaded. Type in /bitemelee for more Options.")
+	SLASH_BITEME1 = "/biteme"
+	SLASH_BITEME2 = "/bm"
+	SlashCmdList["BITEME"] = function(msg) self:SlashCommandHandler(msg)	end	
+	self:message("loaded. Type in /biteme for more Options.")
 	-- Zonen tracken
 	self:RegisterEvent("ZONE_CHANGED")
 	self:RegisterEvent("ZONE_CHANGED_INDOORS")
@@ -60,89 +60,106 @@ function BiteMELEE:Initialize()
 end
 
 -- Interpreter der Slashbefehle, hier 0 = reload ui
-function ImmotaFrames_SlashCommandHandler(msg)
-	if msg == "0" then ReloadUI() end	
- 	self:ToggleFrame(msg);
+function BiteME:SlashCommandHandler(arg)
+	if arg == "0" then ReloadUI() end	
+ 	self:Toggle(arg);
 end
 
-local subzone    = GetSubZoneText()
+local subzone = GetSubZoneText()
 local difficulty = GetInstanceDifficulty()
-function BiteMELEE:CheckZone()
+function BiteME:CheckZone()
 	subzone    = GetSubZoneText()
 	difficulty = GetInstanceDifficulty()
 	if subzone == "EN" or subzone == "DE"  then
-		self:message("Looking for |TInterface\\Icons\\ability_warlock_improvedsoulleech:16|tEssence of the Blood Queen!")
+		self:message("Looking for |TInterface\\Icons\\ability_warlock_improvedsoulch:16|tEssence of the Blood Queen!")
 		self:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 	else			
 		self:UnregisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 	end	
 end
 
-function BiteMELEE:BiteOrder()	
-		if dstName ~= BiteMe[1][1] or dstName ~= BiteMe[2][1] then
+function BiteME:BiteOrder()	
+		if dstName ~= BiteMe["Left"][1] or dstName ~= BiteME["Right"][1] then
 		self:message(format("%s ist kein Verteiler, suche nach anderen", dstName))
-			for k,v in pairs(BiteMe[1]) do
+			for k,v in pairs(BiteMe["Left"]) do
 				if v == dstName then
 					self:message("links gefunden")
-					table.remove(BiteMe[1], k)
-					table.insert(BiteMe[1], 1 , v)
+					table.remove(BiteMe["Left"], k)
+					table.insert(BiteMe["Left"], 1 , v)
 				end
 			end
-			for k,v in pairs(BiteMe[2]) do
+			for k,v in pairs(BiteME["Right"]) do
 				if v == dstName then
 					self:message("rechts gefunden")
-					table.remove(BiteMe[2],k)
-					table.insert(BiteMe[2],1,v)
+					table.remove(BiteME["Right"],k)
+					table.insert(BiteME["Right"],1,v)
 				end
 			end
 		end		
-		-- Ausgabe in einen chan
-		self:channel(format("(Links) %s %s %s %s",BiteMe[1][1],BiteMe[1][2],BiteMe[1][3],BiteMe[1][4]))
-		self:channel(format("(Rechts) %s %s %s %s",BiteMe[2][1],BiteMe[2][2],BiteMe[2][3],BiteMe[2][4]))		
-		-- BEISPIEL
-		--self:whisper("TEXT","PLAYER")
-		--format("%s %s %s %s",var1,var2,var3,var4) -> "bla1 bla2 bla3 bla4"
+		-- Ausgabezeugs
+		
 end
 
--- Funktion f�r den Button Send Setup. Sendet die Aufstellung an den Raidchat
-function BiteMELEE:OnClick(arg)
-	id = this:GetID()
-	out("ImmotaFrames: OnClick: " .. this:GetName() .. " ,ID: " .. id .. " ,Button:" ..arg1)
-	out2ra("Aufstellung Melees Blood Queen")
-	out2ra("Linke Seite: " .. BiteMe["Left"][1].. ", " .. BiteMe["Left"][2] .. ", ".. BiteMe["Left"][3] .. ", " .. BiteMe["Left"][4].. ", " .. BiteMe["Left"][5])
-	out2ra("Rechte Seite: " .. BiteMe["Right"][1].. ", " .. BiteMe["Right"][2] .. ", ".. BiteMe["Right"][3] .. ", " .. BiteMe["Right"][4].. ", " .. BiteMe["Right"][5])
-	out2ra("Verteiler bei den Melees ist " .. BiteMe["Left"][1])
-	out2ra("Verteiler bei den Ranged ist " .. RangedPlayer1)
-	-- out2ra("-->  Hallo Karla :)")
+-- Funktion für den Button Send Setup. Sendet die Aufstellung an den Raidchat
+function BiteME:OnClick(...)
+	self:channel("Aufstellung Mes Blood Queen")
+	self:channel(format("Linke Seite: %s, %s, %s, %s, %s",BiteMe["Left"][1],BiteMe["Left"][2],BiteMe["Left"][3],BiteMe["Left"][4],BiteMe["Left"][5]))
+	self:channel(format("Rechte Seite: %s, %s, %s, %s, %s",BiteMe["Right"][1],BiteMe["Right"][2],BiteMe["Right"][3],BiteMe["Right"][4],BiteMe["Right"][5]))
+	self:channel(format("Verteiler bei den Mes ist %s",BiteMe["Left"][1]))
+	self:channel(format("Verteiler bei den Ranged ist %s",RangedPlayer1))
 end
 
---  HELPER  ZEUGS
-function BiteMELEE:whisper(msg, name)
-   if not (msg and name) then return end   
-   SendChatMessage(Prefix..msg, "WHISPER", nil, name)
-end
-
-function BiteMELEE:channel(msg)
-	if msg then		
-		SendChatMessage(msg, "OFFICER", nil)
-	end	
-end
-
-local print,format = print,string.format
-function BiteMELEE:message(msg)
-	if msg then		
-		print(format("|cffff0033"..Prefix.."|r%s",msg))
+-- Funktion für den Button SendData. Setter Funktionen für die BiteMe Tabelle
+function BiteMe:SendData(...)
+	-- Linke Seite  
+   	if ImmotaFrames1_InputBox1:GetText() then    		
+		BiteMe["Left"][1] = ImmotaFrames1_InputBox1:GetText()
+	end
+	if ImmotaFrames1_InputBox2:GetText() then
+		BiteMe["Left"][2] = ImmotaFrames1_InputBox2:GetText()
+	end
+	if ImmotaFrames1_InputBox3:GetText() then
+		BiteMe["Left"][3] = ImmotaFrames1_InputBox3:GetText()
+	end
+	if ImmotaFrames1_InputBox4:GetText() then
+		BiteMe["Left"][4] = ImmotaFrames1_InputBox4:GetText()		
+	end
+	if ImmotaFrames1_InputBox5:GetText() then
+		BiteMe["Left"][5] = ImmotaFrames1_InputBox5:GetText()
+   	end
+	-- Rechte Seite  
+   	if ImmotaFrames1_InputBox6:GetText() then
+    	BiteMe["Right"][1] = ImmotaFrames1_InputBox6:GetText()
+	end
+   	if ImmotaFrames1_InputBox7:GetText() then
+		BiteMe["Right"][2] = ImmotaFrames1_InputBox7:GetText()
+	end
+	if ImmotaFrames1_InputBox8:GetText() then
+		BiteMe["Right"][3] = ImmotaFrames1_InputBox8:GetText()
+	end
+	if ImmotaFrames1_InputBox9:GetText() then
+		BiteMe["Right"][4] = ImmotaFrames1_InputBox9:GetText()		
+	end
+	if ImmotaFrames1_InputBox10:GetText() then
+		BiteMe["Right"][5] = ImmotaFrames1_InputBox10:GetText()
+	end
+	-- Ranged
+	if ImmotaFrames1_InputBox11:GetText() then
+		RangedPlayer1 = ImmotaFrames1_InputBox11:GetText()
+	end
+	if ImmotaFrames1_InputBox12:GetText() then
+		RangedPlayer2 = ImmotaFrames1_InputBox12:GetText()
 	end
 end
 
---  Chat-Filter
-function filterOutgoing(self, event, ...)
-	local msg = ...
-	if not msg and self then
-		return filterOutgoing(nil, nil, self, event)
+-- Interpreter der Slashbefehle2.
+function BiteME:Toggle()
+	local frame = getglobal("BiteMe")
+	if frame then
+		if frame:IsVisible() then
+			frame:Hide()
+		else
+			frame:Show()
+		end
 	end
-	-- wir gucken ob der anfang der msg gleich unserem Prefix ist
-	return msg:sub(1, Prefix:len()) == Prefix, ...	
 end
--- Filter muss noch registriert werden
-ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER_INFORM", filterOutgoing)
---
+++ helper.lua
@@ -0,0 +1,30 @@
+﻿--  HELPER  ZEUGS
+function BiteMELEE:whisper(msg, name)
+   if not (msg and name) then return end   
+   SendChatMessage(Prefix..msg, "WHISPER", nil, name)
+end
+
+function BiteMELEE:channel(msg)
+	if msg then		
+		SendChatMessage(msg, "OFFICER", nil)
+	end	
+end
+
+local print,format = print,string.format
+function BiteMELEE:message(msg)
+	if msg then		
+		print(format("|cffff0033"..Prefix.."|r%s",msg))
+	end
+end
+
+--  Chat-Filter
+function filterOutgoing(self, event, ...)
+	local msg = ...
+	if not msg and self then
+		return filterOutgoing(nil, nil, self, event)
+	end
+	-- wir gucken ob der anfang der msg gleich unserem Prefix ist
+	return msg:sub(1, Prefix:len()) == Prefix, ...	
+end
+-- Filter muss noch registriert werden
+ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER_INFORM", filterOutgoing)
