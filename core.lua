182d2c2ffabccb91897f74b19d039ab36de90f3d
Robert <robroyal@gmx.net>
2010-03-08 18:05:46 +0100

    zeugs

--- core.lua
+++ core.lua
@@ -1,22 +1,37 @@
 ﻿-- Einstellungen
 local BiteMe = {}
-BiteMe["Left"] = {
-	"L1", --L1
-	"L2", --L2
-	"Knopf", --L3
-	"L4",  --L4
-	"L5"  --L5
+BiteMe["Melee"] = {}
+BiteMe["Melee"]["Left"] = {
+	"ML1", --L1
+	"ML2", --L2
+	"ML3", --L3
+	"ML4",  --L4
+	"ML5"  --L5
 }
-BiteMe["Right"] = {
-	"R1", --R1
-	"R2", --R2
-	"R3", --R3
-	"R4", --R4
-	"R5"  --R5
+BiteMe["Melee"]["Right"] = {
+	"MR1", --R1
+	"MR2", --R2
+	"MR3", --R3
+	"MR4", --R4
+	"MR5"  --R5
 }
-local RangedPlayer1 = "RP1"
-local RangedPlayer2 = "RP2"
+BiteMe["Ranged"] = {}
+BiteMe["Ranged"]["Left"] = {
+	"RL1", --RL1
+	"RL2", --RL2
+	"RL3", --RL3
+	"RL4", --RL4
+	"RL5"  --L5
+}
+BiteMe["Ranged"]["Right"] = {
+	"RR1", --RR1
+	"RR2", --RR2
+	"RR3", --RR3
+	"RR4", --RR4
+	"RR5"  --RR5
+}
 local Prefix = "BiteME: " -- Pröfix für z.B. die Whispers
+local reordered = false
 
 -- alle unsere Combatlog Daten
 local timestamp, type, srcGUID, srcName, srcFlgs, dstGUID, dstName, dstFlgs, spellID, spellName
@@ -31,13 +46,14 @@ BiteME:SetScript("OnEvent",
 			--  AUREN    
 			if type == "SPELL_AURA_APPLIED" or type == "SPELL_AURA_APPLIED_DOSE" then
 				spellID, spellName = select(9, ...)				
-				if spellID == 71473 then  -- Essence of the Blood Queen			
-					self:BiteOrder()
-					self:UnregisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+				if spellID == 71473 then  -- Essence of the Blood Queen
+					if not reordered then self:BiteOrder() end
+					self:Announce()
 				end
 			end
 		elseif event:sub(1,12) == "ZONE_CHANGED" then
 			--self:message(format("Zone changed to %q",GetSubZoneText()))
+			reordered = false -- wir mißbrauchen das Event zum resetten
 			self:CheckZone()
 		elseif event == "ADDON_LOADED" then
 			self:Initialize()
@@ -50,7 +66,7 @@ function BiteME:Initialize()
 	-- Slashcommands einbinden
 	SLASH_BITEME1 = "/biteme"
 	SLASH_BITEME2 = "/bm"
-	SlashCmdList["BITEME"] = function(msg) self:SlashCommandHandler(msg)	end	
+	SlashCmdList["BITEME"] = function(msg) self:SlashCommandHandler(msg) end	
 	self:message("loaded. Type in /biteme for more Options.")
 	-- Zonen tracken
 	self:RegisterEvent("ZONE_CHANGED")
@@ -78,42 +94,56 @@ function BiteME:CheckZone()
 	end	
 end
 
-function BiteME:BiteOrder()	
-		if dstName ~= BiteMe["Left"][1] or dstName ~= BiteME["Right"][1] then
+function BiteME:BiteOrder()		
+	-- Ist der erste gebissene kein Verteiler? Dann verändern wir die Reihenfolgen
+	if  dstName ~= BiteMe["Melee"]["Left"][1] or dstName ~= BiteMe["Melee"]["Right"][1] or dstName ~= BiteMe["Ranged"]["Left"][1] or dstName ~= BiteMe["Ranged"]["Right"][1] then
 		self:message(format("%s ist kein Verteiler, suche nach anderen", dstName))
-			for k,v in pairs(BiteMe["Left"]) do
-				if v == dstName then
-					self:message("links gefunden")
-					table.remove(BiteMe["Left"], k)
-					table.insert(BiteMe["Left"], 1 , v)
-				end
+		-- Melees links
+		for k,v in pairs(BiteMe["Melee"]["Left"]) do
+			if v == dstName then					
+				table.remove(BiteMe["Melee"]["Left"], k)
+				table.insert(BiteMe["Melee"]["Left"], 1 , v)
 			end
-			for k,v in pairs(BiteME["Right"]) do
-				if v == dstName then
-					self:message("rechts gefunden")
-					table.remove(BiteME["Right"],k)
-					table.insert(BiteME["Right"],1,v)
-				end
+		end
+		-- Melees rechts
+		for k,v in pairs(BiteMe["Melee"]["Right"]) do
+			if v == dstName then					
+				table.remove(BiteMe["Melee"]["Right"],k)
+				table.insert(BiteMe["Melee"]["Right"],1,v)
 			end
-		end		
-		-- Ausgabezeugs
-		
+		end
+		-- Ranged links
+		for k,v in pairs(BiteMe["Ranged"]["Left"]) do
+			if v == dstName then
+				BiteMe["Ranged"]["Left"][k] = BiteMe["Ranged"]["Left"][1]
+				BiteMe["Ranged"]["Left"][1] = dstName
+			end
+		end
+		-- Ranged rechts
+		for k,v in pairs(BiteMe["Ranged"]["Right"]) do
+			if v == dstName then
+				BiteMe["Ranged"]["Right"][k] = BiteMe["Ranged"]["Right"][1]
+				BiteMe["Ranged"]["Right"][1] = dstName
+			end
+		end
+	end
+	reordered = true
 end
 
 -- Funktion für den Button GetTarget
 function BiteME:GetTarget(...)
 	if UnitName("target") then
-	self:channel("Target: "..UnitName("target"))
-	BiteMe_InputBox1:Insert(UnitName("target"))
+		self:channel("Target: "..UnitName("target"))
+		BiteMe_InputBox1:Insert(UnitName("target"))
 	end
 end
 
 -- Funktion für den Button Send Setup. Sendet die Aufstellung an den Raidchat
 function BiteME:OnClick()
 	self:channel("Aufstellung Melees Blood Queen")
-	self:channel(format("Linke Seite: %s, %s, %s, %s, %s",BiteMe["Left"][1],BiteMe["Left"][2],BiteMe["Left"][3],BiteMe["Left"][4],BiteMe["Left"][5]))
-	self:channel(format("Rechte Seite: %s, %s, %s, %s, %s",BiteMe["Right"][1],BiteMe["Right"][2],BiteMe["Right"][3],BiteMe["Right"][4],BiteMe["Right"][5]))
-	self:channel(format("Verteiler bei den Mes ist %s",BiteMe["Left"][1]))
+	self:channel(format("Linke Seite: %s, %s, %s, %s, %s",BiteMe["Melee"]["Left"][1],BiteMe["Melee"]["Left"][2],BiteMe["Melee"]["Left"][3],BiteMe["Melee"]["Left"][4],BiteMe["Melee"]["Left"][5]))
+	self:channel(format("Rechte Seite: %s, %s, %s, %s, %s",BiteMe["Melee"]["Right"][1],BiteMe["Melee"]["Right"][2],BiteMe["Melee"]["Right"][3],BiteMe["Melee"]["Right"][4],BiteMe["Melee"]["Right"][5]))
+	self:channel(format("Verteiler bei den Mes ist %s",BiteMe["Melee"]["Left"][1]))
 	self:channel(format("Verteiler bei den Ranged ist %s",RangedPlayer1))
 end
 
@@ -121,35 +151,35 @@ end
 function BiteME:SendData()
 	-- Linke Seite  
    	if BiteMe_InputBox1:GetText() then    		
-		BiteMe["Left"][1] = BiteMe_InputBox1:GetText()
+		BiteMe["Melee"]["Left"][1] = BiteMe_InputBox1:GetText()
 	end
 	if BiteMe_InputBox2:GetText() then
-		BiteMe["Left"][2] = BiteMe_InputBox2:GetText()
+		BiteMe["Melee"]["Left"][2] = BiteMe_InputBox2:GetText()
 	end
 	if BiteMe_InputBox3:GetText() then
-		BiteMe["Left"][3] = BiteMe_InputBox3:GetText()
+		BiteMe["Melee"]["Left"][3] = BiteMe_InputBox3:GetText()
 	end
 	if BiteMe_InputBox4:GetText() then
-		BiteMe["Left"][4] = BiteMe_InputBox4:GetText()		
+		BiteMe["Melee"]["Left"][4] = BiteMe_InputBox4:GetText()		
 	end
 	if BiteMe_InputBox5:GetText() then
-		BiteMe["Left"][5] = BiteMe_InputBox5:GetText()
+		BiteMe["Melee"]["Left"][5] = BiteMe_InputBox5:GetText()
    	end
 	-- Rechte Seite  
    	if BiteMe_InputBox6:GetText() then
-    	BiteMe["Right"][1] = BiteMe_InputBox6:GetText()
+    	BiteMe["Melee"]["Right"][1] = BiteMe_InputBox6:GetText()
 	end
    	if BiteMe_InputBox7:GetText() then
-		BiteMe["Right"][2] = BiteMe_InputBox7:GetText()
+		BiteMe["Melee"]["Right"][2] = BiteMe_InputBox7:GetText()
 	end
 	if BiteMe_InputBox8:GetText() then
-		BiteMe["Right"][3] = BiteMe_InputBox8:GetText()
+		BiteMe["Melee"]["Right"][3] = BiteMe_InputBox8:GetText()
 	end
 	if BiteMe_InputBox9:GetText() then
-		BiteMe["Right"][4] = BiteMe_InputBox9:GetText()		
+		BiteMe["Melee"]["Right"][4] = BiteMe_InputBox9:GetText()		
 	end
 	if BiteMe_InputBox10:GetText() then
-		BiteMe["Right"][5] = BiteMe_InputBox10:GetText()
+		BiteMe["Melee"]["Right"][5] = BiteMe_InputBox10:GetText()
 	end
 	-- Ranged
 	if BiteMe_InputBox11:GetText() then
